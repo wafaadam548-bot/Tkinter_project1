f4bdcc335368f2fc2c1abafa57f3021aa6c100d0
wafaadam548 <wafaadam548@gmail.com>
2026-04-23 01:04:25 -0400

    test the Label

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tkinter as tk
-window = tk.Tk()
+from tkinter import *
+window = Tk()
 width=500
 height=500
 window.geometry(f"{width}x{height}")
+label=Label(window , text=("Hi there "))
+label.grid(row=0,column=2)
 window.mainloop()
