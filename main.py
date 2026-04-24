da1e17f4bebe762409dd1648c8aac3446a367f2e
wafaadam548 <wafaadam548@gmail.com>
2026-04-23 21:48:01 -0400

    making button to new screen

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ window = Tk()
 width=500
 height=500
 window.geometry(f"{width}x{height}")
-label=Label(window , text=("Hi there "))
-label.grid(row=0,column=2)
+def hi():
+    label=Label(window , text=("Hi there "))
+    label.grid(row=4,column=4)
+start=Button(window, text=("start",font=("Arial",30)),command=hi)
+start.grid(row=0,column=0,columnspan=4)
 window.mainloop()
