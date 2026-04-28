e00da83df3031f0fd980a8cafd394e41fbe0e7e1
wafaadam548 <wafaadam548@gmail.com>
2026-04-28 09:16:41 -0400

    Write the choice of the game that the player will choose

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from tkinter import *
-window = Tk()
-width=500
-height=500
-window.geometry(f"{width}x{height}")
-def hi():
-    label=Label(window , text=("Hi there "))
-    label.grid(row=4,column=4)
-start=Button(window, text=("start",font=("Arial",30)),command=hi)
-start.grid(row=0,column=0,columnspan=4)
-window.mainloop()
+print(""" Welcome to our gaming world!. We have many many games options. Chaose any game you want to play and enjoy your time  """)
+choice=input("""Enter your choice:
+1/Mad libs
+2/ Mastermind
+3/Mental math game 
+4/Trivia game 
+5/ Word gussing name
+6/ Escape room """)
+score=20
+print("hint : Sometimes the safest way forward is the one less seen!!!")
+while score>0:
+    print(f"{choice}: with all these money I am going to be rich and achive all my dreams")
